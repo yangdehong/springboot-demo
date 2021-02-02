40dcebfd4ef0edbfda376522f69ae643c971a977
yangdehong <405104787@qq.com>
2021-02-02 20:35:39 +0800

    redis分布式锁

--- z_redis/src/main/java/com/ydh/redsheep/redis/controller/LockController.java
+++ z_redis/src/main/java/com/ydh/redsheep/redis/controller/LockController.java
@@ -1,7 +1,6 @@
 package com.ydh.redsheep.redis.controller;
 
-import com.ydh.redsheep.lock.AquiredLockWorker;
-import com.ydh.redsheep.lock.RedisLocker;
+import com.ydh.redsheep.lock.DistributedRedisLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,62 +17,18 @@ import java.util.concurrent.CountDownLatch;
 @RequestMapping("lock")
 public class LockController {
 
-    @Autowired
-    RedisLocker distributedLocker;
     @RequestMapping(value = "/redlock")
     public String testRedlock() throws Exception{
+        String key = "lock001";
+        //加锁
+        DistributedRedisLock.acquire(key);
+        //执行具体业务逻辑
+        System.out.println("分布式锁");
+        //释放锁
+        DistributedRedisLock.release(key);
 
-        CountDownLatch startSignal = new CountDownLatch(1);
-        CountDownLatch doneSignal = new CountDownLatch(5);
-        for (int i = 0; i < 5; ++i) { // create and start threads
-            new Thread(new Worker(startSignal, doneSignal)).start();
-        }
-        startSignal.countDown(); // let all threads proceed
-        doneSignal.await();
-        System.out.println("All processors done. Shutdown connection");
-        return "redlock";
+        return "";
     }
 
-    class Worker implements Runnable {
-        private final CountDownLatch startSignal;
-        private final CountDownLatch doneSignal;
-
-        Worker(CountDownLatch startSignal, CountDownLatch doneSignal) {
-            this.startSignal = startSignal;
-            this.doneSignal = doneSignal;
-        }
-
-        public void run() {
-//            doTask();
-            try {
-                startSignal.await();
-                distributedLocker.lock("test",new AquiredLockWorker<Object>() {
-
-                    @Override
-                    public Object invokeAfterLockAquire() {
-                        doTask();
-                        return null;
-                    }
-
-                });
-            }catch (Exception e){
-
-            }
-        }
-
-        void doTask() {
-            System.out.println(Thread.currentThread().getName() + " start");
-            Random random = new Random();
-            int _int = random.nextInt(200);
-            System.out.println(Thread.currentThread().getName() + " sleep " + _int + "millis");
-            try {
-                Thread.sleep(_int);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println(Thread.currentThread().getName() + " end");
-            doneSignal.countDown();
-        }
-    }
 
 }
