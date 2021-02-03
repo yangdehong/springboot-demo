016dd95257abb5cd5a473912ebc9089eee37f897
yangdehong <405104787@qq.com>
2021-02-03 18:18:06 +0800

    jediscluster

---
+++ z_redis/src/main/java/com/ydh/redsheep/cluster/ClusterTest.java
@@ -0,0 +1,27 @@
+package com.ydh.redsheep.cluster;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ClusterTest {
+
+    public static void main(String[] args) {
+        JedisPoolConfig config = new JedisPoolConfig();
+        Set<HostAndPort> jedisClusterNode = new HashSet<>();
+        jedisClusterNode.add(new HostAndPort("172.16.131.10", 7001));
+        jedisClusterNode.add(new HostAndPort("172.16.131.10", 7002));
+        jedisClusterNode.add(new HostAndPort("172.16.131.10", 7003));
+        jedisClusterNode.add(new HostAndPort("172.16.131.10", 7004));
+        jedisClusterNode.add(new HostAndPort("172.16.131.10", 7005));
+        jedisClusterNode.add(new HostAndPort("172.16.131.10", 7006));
+        JedisCluster jcd = new JedisCluster(jedisClusterNode, config);
+        jcd.set("name:099","testkey");
+        String value = jcd.get("name:099");
+        System.out.println(value);
+    }
+
+}
