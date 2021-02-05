b4c0fc307dac37d20f2e92c40851308e9d35d360
yangdehong <405104787@qq.com>
2021-02-05 16:10:53 +0800

    springboot+rabbit

--- z_rabbitmq/src/main/java/com/ydh/redsheep/comsumer/MyMessageListener.java
+++ z_rabbitmq/src/main/java/com/ydh/redsheep/comsumer/MyMessageListener.java
@@ -1,33 +1,47 @@
 package com.ydh.redsheep.comsumer;
 
+import com.rabbitmq.client.Channel;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
 
 @Component
 public class MyMessageListener {
 
-    /**
-     * com.rabbitmq.client.Channel channel对象
-     * org.springframework.amqp.core.Message message对象 可以直接操作原生的AMQP消息
-     * org.springframework.messaging.Message to use the messaging abstraction counterpart
-     * @Payload 注解方法参数，改参数的值就是消息体
-     * @Header 注解方法参数，访问指定的消息头字段的值
-     * @Headers 该注解的方法参数获取该消息的消息头的所有字段，参数类型对应于map集合。
-     * MessageHeaders 参数类型，访问所有消息头字段
-     * MessageHeaderAccessor or AmqpMessageHeaderAccessor 访问所有消息头字段
-     */
-//    @RabbitListener(queues = "queue.anno")
-//    public void whenMessageCome(Message message) throws UnsupportedEncodingException {
-//        System.out.println(new String(message.getBody(), message.getMessageProperties().getContentEncoding()));
+//    @RabbitListener(queues = "queue.boot")
+//    public void getMyMessage(@Payload String message, @Header(name = "hello") String value, Channel channel) {
+//        System.out.println(message);
+//        System.out.println("hello = " + value);
+//
+//        // 确认消息
+//        channel.basicAck();
+//        // 拒收消息
+//        channel.basicReject();
 //    }
 
-    @RabbitListener(queues = "queue.anno")
-    public void whenMessageCome(@Payload String messageStr) {
-        System.out.println(messageStr);
+    private Integer index = 0;
+
+    @RabbitListener(queues = "queue.boot")
+    public void getMyMessage(Message message, Channel channel) throws IOException {
+        String value = message.getMessageProperties().getHeader("hello");
+
+        System.out.println(message);
+        System.out.println("hello = " + value);
+
+        final long deliveryTag = message.getMessageProperties().getDeliveryTag();
+
+        if (index % 2 == 0) {
+            // 确认消息
+            channel.basicAck(deliveryTag, false);
+        } else {
+            // 拒收消息
+            channel.basicReject(deliveryTag, false);
+        }
+        index++;
     }
 
 }
--- z_rabbitmq/src/main/java/com/ydh/redsheep/config/RabbitConfig.java
+++ z_rabbitmq/src/main/java/com/ydh/redsheep/config/RabbitConfig.java
@@ -1,83 +1,33 @@
 package com.ydh.redsheep.config;
 
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.net.URI;
-
-@ComponentScan("com.ydh.redsheep.comsumer")
 @Configuration
-@EnableRabbit  // xml中也可以使用<rabbit:annotation-driven />  启用@RabbitListener注解
 public class RabbitConfig {
 
-    // 连接工厂
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        ConnectionFactory factory = new CachingConnectionFactory(URI.create("amqp://root:123456@172.16.131.16:5672/%2f"));
-        return factory;
-    }
-
-    // RabbitTemplate
-    @Bean
-    @Autowired
-    public RabbitTemplate rabbitTemplate(ConnectionFactory factory) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(factory);
-        return rabbitTemplate;
-    }
-
-    // RabbitAdmin
-    @Bean
-    @Autowired
-    public RabbitAdmin rabbitAdmin(ConnectionFactory factory) {
-        RabbitAdmin rabbitAdmin = new RabbitAdmin(factory);
-        return rabbitAdmin;
-    }
-
-    // Queue
     @Bean
     public Queue queue() {
-        final Queue queue = QueueBuilder.nonDurable("queue.anno").build();
-        return queue;
+        return new Queue("queue.boot", false, false, false, null);
     }
 
-    // Exchange
     @Bean
     public Exchange exchange() {
-        final FanoutExchange fanoutExchange = new FanoutExchange("ex.anno.fanout", false, false, null);
-        return fanoutExchange;
+        return new TopicExchange("ex.boot", false, false, null);
     }
 
-    // Binding
+
     @Bean
-    @Autowired
-    public Binding binding(Queue queue, Exchange exchange) {
-        // 创建一个绑定，不指定绑定的参数
-        final Binding binding = BindingBuilder.bind(queue).to(exchange).with("key.anno").noargs();
-        return binding;
+    public Binding binding() {
+        return new Binding("queue.boot",
+                Binding.DestinationType.QUEUE,
+                "ex.boot",
+                "key.boot",
+                null);
     }
 
-    @Bean("rabbitListenerContainerFactory")
-    @Autowired
-    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
-//        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
-//        factory.setAcknowledgeMode(AcknowledgeMode.NONE);
-//        factory.setConcurrentConsumers(10);
-//        factory.setMaxConcurrentConsumers(15);
-        // 按照批次消费消息
-//        factory.setBatchSize(10);
-
-        return factory;
-    }
 }
