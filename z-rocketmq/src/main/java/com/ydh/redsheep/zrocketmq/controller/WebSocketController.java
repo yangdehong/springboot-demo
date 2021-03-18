9b311195f0b87d2f8bfaba65c2b9f6e6ffd263f7
yangdehong <405104787@qq.com>
2021-03-18 18:23:56 +0800

    ws

---
+++ z-rocketmq/src/main/java/com/ydh/redsheep/zrocketmq/controller/WebSocketController.java
@@ -0,0 +1,27 @@
+package com.ydh.redsheep.zrocketmq.controller;
+
+import com.ydh.redsheep.zrocketmq.ws.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebSocketController {
+
+    @MessageMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    //@MessageMapping是消息转发器，对应配置文件的setApplicationDestinationPrefixes
+    //                 /app，客户端可以往"/app/hello"发送消息，该注解就会接受消息，交给greeting（）方法处理
+    //SentTo对应配置文件的enableSimpleBroker("/topic"); 客户端会订阅stompClient.subscribe('/topic/greetings'）这个通道的消息
+    //                 发往该通道的消息，都会被订阅了该通道的客户端接受到。没有订阅的则不接受
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return "Hello, " + message.getName() + "!";
+    }
+
+}
---
+++ z-rocketmq/src/main/java/com/ydh/redsheep/zrocketmq/ws/HelloMessage.java
@@ -0,0 +1,10 @@
+package com.ydh.redsheep.zrocketmq.ws;
+
+import lombok.Data;
+
+@Data
+public class HelloMessage {
+
+    private String name;
+
+}
