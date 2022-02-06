8a2adaca5c1b13c3e830951f92b0941d172d9b76
yangdehong <405104787@qq.com>
2022-02-06 11:28:46 +0800

    动态数据源

--- database/src/main/java/com/ydh/redsheep/database/DatabaseApplication.java
+++ database/src/main/java/com/ydh/redsheep/database/DatabaseApplication.java
@@ -3,9 +3,10 @@ package com.ydh.redsheep.database;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @MapperScan(basePackages = "com.ydh.redsheep.database.mapper")
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class DatabaseApplication {
 
     public static void main(String[] args) {
--- database/src/main/java/com/ydh/redsheep/database/controller/UserController.java
+++ database/src/main/java/com/ydh/redsheep/database/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.ydh.redsheep.database.controller;
 
+import com.ydh.redsheep.database.common.aop.RoutingWith;
+import com.ydh.redsheep.database.common.config.RoutingDataSourceContext;
 import com.ydh.redsheep.database.mapper.UserMapper;
 import com.ydh.redsheep.database.pojo.User;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @description:
@@ -24,6 +27,21 @@ public class UserController {
     public User get(){
         User user = userMapper.selectById(2L);
         return user;
+    }
+
+    @RoutingWith("masterDataSource")
+    @GetMapping("/findAllM")
+    public String findAllProductM() {
+        List<User> all = userMapper.findAll();
+        System.out.println(all);
+        return "ydh";
+    }
+    @RoutingWith("slaveDataSource")
+    @GetMapping("/findAllS")
+    public String findAllProductS() {
+        List<User> all = userMapper.findAll();
+        System.out.println(all);
+        return "ydh2";
     }
 
 }
