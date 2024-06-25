5f0f860aa4ee8feaa15885c5cf117706ff6c6d7d
yangdehong <405104787@qq.com>
2024-06-25 16:50:00 +0800

    redis

--- cache/src/test/java/com/ydh/redsheep/CacheApplicationTests.java
+++ cache/src/test/java/com/ydh/redsheep/CacheApplicationTests.java
@@ -1,14 +1,11 @@
 package com.ydh.redsheep;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class CacheApplicationTests {
 
